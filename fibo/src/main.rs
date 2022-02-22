9a6aa62bea5ad9e54c69d3379efccccaec171312
thomas auguste <thomas.auguste92@gmail.com>
2022-02-22 16:38:01 +0100

    arithmétique saturée

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
-    for i in 0..=42 {
+    for i in 0..=50 {
         println!("fibo({}) = {}", i, fibo(i))
     }
 }
 
 fn fibo(n: u32) -> u32 {
-    let mut new_val = 1;
-    let mut former_val = 0;
-    let mut inter;
+    let mut new_val: u32 = 1;
+    let mut former_val: u32 = 0;
+    let mut inter: u32;
     if n == 0 {
         return 0;
     } else if n == 1 {
@@ -15,7 +15,7 @@ fn fibo(n: u32) -> u32 {
     }
     for _ in 1..n {
         inter = new_val;
-        new_val = new_val + former_val;
+        new_val = new_val.saturating_add(former_val);
         former_val = inter;
     }
     new_val
