3efab6a179a764e9dc704e2414b293d0b7efbf6d
thomas auguste <thomas.auguste92@gmail.com>
2022-02-22 17:04:10 +0100

    with options

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,21 +1,26 @@
 fn main() {
     for i in 0..=50 {
-        println!("fibo({}) = {}", i, fibo(i))
+        let val: Option<u32> = fibo(i);
+        match val {
+            None => return,
+            _    => println!("fibo({}) = {}", i, val.unwrap())
+        }
+        
     }
 }
 
-fn fibo(n: u32) -> u32 {
-    let mut new_val: u32 = 1;
-    let mut former_val: u32 = 0;
-    let mut inter: u32;
+fn fibo(n: u32) -> Option<u32> {
+    let mut new_val: Option<u32> = Some(1);
+    let mut former_val: Option<u32> = Some(0);
+    let mut inter: Option<u32>;
     if n == 0 {
-        return 0;
+        return Some(0);
     } else if n == 1 {
-        return 1;
+        return Some(1);
     }
     for _ in 1..n {
         inter = new_val;
-        new_val = new_val.checked_add(former_val).unwrap();
+        new_val = new_val.unwrap().checked_add(former_val.unwrap());
         former_val = inter;
     }
     new_val
