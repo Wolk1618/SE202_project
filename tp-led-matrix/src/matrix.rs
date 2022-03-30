5281e3fb11c9d0304bb29606bb38f38b17801eab
thomas auguste <thomas.auguste92@gmail.com>
2022-03-31 01:04:04 +0200

    ongoing

---
+++ tp-led-matrix/src/matrix.rs
@@ -0,0 +1,95 @@
+
+use stm32l4xx_hal::gpio::*;
+use tp_led_matrix::{Color, Image};
+
+pub struct Matrix {
+    sb: PC5<Output<PushPull>>,
+    lat: PC4<Output<PushPull>>,
+    rst: PC3<Output<PushPull>>,
+    sck: PB1<Output<PushPull>>,
+    sda: PA4<Output<PushPull>>,
+    c0: PB2<Output<PushPull>>,
+    c1: PA15<Output<PushPull>>,
+    c2: PA2<Output<PushPull>>,
+    c3: PA7<Output<PushPull>>,
+    c4: PA6<Output<PushPull>>,
+    c5: PA5<Output<PushPull>>,
+    c6: PB0<Output<PushPull>>,
+    c7: PA3<Output<PushPull>>,
+}
+
+impl Matrix {
+    /// Create a new matrix from the control registers and the individual
+    /// unconfigured pins. SB and LAT will be set high by default, while
+    /// other pins will be set low. After 100ms, RST will be set high, and
+    /// the bank 0 will be initialized by calling `init_bank0()` on the
+    /// newly constructed structure.
+    /// The pins will be set to very high speed mode.
+    #[allow(clippy::too_many_arguments)]   // Necessary to avoid a clippy warning
+    pub fn new(
+        pa2: PA2<Analog>,
+        pa3: PA3<Analog>,
+        pa4: PA4<Analog>,
+        pa5: PA5<Analog>,
+        pa6: PA6<Analog>,
+        pa7: PA7<Analog>,
+        pa15: PA15<Alternate<PushPull, 0>>,
+        pb0: PB0<Analog>,
+        pb1: PB1<Analog>,
+        pb2: PB2<Analog>,
+        pc3: PC3<Analog>,
+        pc4: PC4<Analog>,
+        pc5: PC5<Analog>,
+        gpioa_moder: &mut MODER<'A'>,
+        gpioa_otyper: &mut OTYPER<'A'>,
+        gpiob_moder: &mut MODER<'B'>,
+        gpiob_otyper: &mut OTYPER<'B'>,
+        gpioc_moder: &mut MODER<'C'>,
+        gpioc_otyper: &mut OTYPER<'C'>,
+        clocks: Clocks,
+    ) -> Self {
+        // Use .into_push_pull_output_in_state(…) to set an initial state on pins
+        todo!()
+    }
+
+    /// Make a brief high pulse of the SCK pin
+    fn pulse_sck(&mut self) {
+        todo!()
+    }
+
+    /// Make a brief low pulse of the LAT pin
+    fn pulse_lat(&mut self) {
+        todo!()
+    }
+
+    /// Set the given row output in the chosen state
+    fn row(&mut self, row: usize, state: PinState) {
+        todo!()
+    }
+
+    /// Send a byte on SDA starting with the MSB and pulse SCK high after each bit
+    fn send_byte(&mut self, pixel: u8) {
+        todo!()
+    }
+
+    /// Send a full row of bytes in BGR order and pulse LAT low. Gamma correction
+    /// must be applied to every pixel before sending them. The previous row must
+    /// be deactivated and the new one activated.
+    pub fn send_row(&mut self, row: usize, pixels: &[Color]) {
+        todo!()
+    }
+
+    /// Initialize bank0 by temporarily setting SB to low and sending 144 one bits,
+    /// pulsing SCK high after each bit and pulsing LAT low at the end. SB is then
+    /// restored to high.
+    fn init_bank0(&mut self) {
+        todo!()
+    }
+
+    /// Display a full image, row by row, as fast as possible.
+    pub fn display_image(&mut self, image: &Image) {
+        // Do not forget that image.row(n) gives access to the content of row n,
+        // and that self.send_row() uses the same format.
+        todo!()
+    }
+}
