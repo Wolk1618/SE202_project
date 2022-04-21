e30df1f1fcbba13a713872d8a54c6e2e018b1503
thomas auguste <thomas.auguste92@gmail.com>
2022-04-22 00:23:34 +0200

    first part serial

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -3,13 +3,15 @@
 #![allow(clippy::empty_loop)]
 
 use stm32l4xx_hal::{pac, prelude::*};
+use stm32l4xx_hal::serial::{Config, Event, Rx, Serial};
 use panic_probe as _;
 use defmt_rtt as _;
 use tp_led_matrix::{Image, Color, matrix::Matrix};
 use dwt_systick_monotonic::DwtSystick;
 use dwt_systick_monotonic::ExtU32;
+use crate::pac::USART1;
 
-#[rtic::app(device = pac, dispatchers = [USART2])]
+#[rtic::app(device = pac, dispatchers = [USART2, USART3])]
 mod app {
 
     use super::*;
@@ -63,21 +65,33 @@ mod app {
             clocks
         );
 
-        let image = Image::gradient(Color::BLUE);
+        let tx = gpiob.pb6.into_alternate::<7>(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
+        let rx = gpiob.pb7.into_alternate::<7>(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
+
+        let config = Config::default().baudrate(38_400.bps());
+        let mut serial = Serial::usart1(dp.USART1, (tx, rx), config, clocks, &mut rcc.apb2);
+        let image = Image::default();
+
+        serial.listen(Event::Rxne);
+
+        let usart1_rx = serial.split().1;
 
         display::spawn(mono.now()).unwrap();
+        rotate_image::spawn(mono.now(), 0).unwrap();
 
         // Return the resources and the monotonic timer
-        (Shared {}, Local { matrix, image }, init::Monotonics(mono))
+        (Shared {image}, Local {matrix, usart1_rx }, init::Monotonics(mono))
     }
 
     #[shared]
-    struct Shared {}
+    struct Shared {
+        image : Image,
+    }
 
     #[local]
     struct Local {
         matrix: Matrix,
-        image : Image,
+        usart1_rx : Rx<USART1>,
     }
 
     #[idle(local = [])]
@@ -109,21 +123,32 @@ mod app {
         }
     }
 
-    #[task(local = [matrix, image, next_line: usize = 0])]
-    fn display(cx: display::Context, at: Instant) {
+    #[task(local = [matrix, next_line: usize = 0], shared = [image], priority = 2)]
+    fn display(mut cx: display::Context, at: Instant) {
         // Display line next_line (cx.local.next_line) of
         // the image (cx.local.image) on the matrix (cx.local.matrix).
         // All those are mutable references.
-        cx.local.matrix.send_row(*cx.local.next_line, cx.local.image.row(*cx.local.next_line));
+        cx.shared.image.lock( |image| {
+            cx.local.matrix.send_row(*cx.local.next_line, image.row(*cx.local.next_line));
+        } );
         // Increment next_line up to 7 and wraparound to 0
-        if *cx.local.next_line == 7 {
-            *cx.local.next_line = 0;
-        } else {
-            *cx.local.next_line += 1;
-        }
+        *cx.local.next_line = (*cx.local.next_line + 1) % 8;
 
         let time : Instant = at + 1.secs() / (60 * 8);
         display::spawn_at(time, time).unwrap();
+    }
+
+    #[task(shared = [image], priority = 1)]
+    fn rotate_image(mut cx: rotate_image::Context, at: Instant, color_index: usize) {
+        cx.shared.image.lock( |image| {
+            match color_index {
+                0 => *image = Image::gradient(Color::RED),
+                1 => *image = Image::gradient(Color::GREEN),
+                2 => *image = Image::gradient(Color::BLUE),
+                _ => panic!("Wrong color value"),
+            }
+        } );
+        rotate_image::spawn_after(1.secs(), at, (color_index + 1) % 3).unwrap();
     }
 
 }
